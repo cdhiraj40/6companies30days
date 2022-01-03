7189462d1b40491e9a3dda32744c88274f9523f5
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-01-03 07:31:58 +0530

    2 questions done

---
+++ week1/question1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<vector<string>> Anagrams(vector<string>& string_list) {
+    unordered_map<string, vector<string>> m;
+    for(auto & x : string_list) {
+        string a = x;
+        std::sort(x.begin(), x.end());
+        m[x].push_back(a);
+    }
+    vector<vector<string>> ans(m.size());
+    for (const auto& mm:m) {
+        auto v = mm.second;
+        ans.push_back(v);
+    }
+    return ans;
+}
+
+int main() {
+    vector<vector<string>> intervals;
+    vector <string>words = {"act","god","cat","dog","tac"};
+
+    intervals = Anagrams(words);
+
+    for(const auto& interval:intervals){
+    for(const auto& num:interval)
+    {
+        cout<<num<<" ";
+    }
+}
+    return 0;
+}
