fb07e142cd718262f00806c7dedbd43cd37f7683
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-01-08 19:49:20 +0530

    amzaon question started

---
+++ AMAZON/question1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> max_of_subarrays(vector<int> arr, int n, int k) {
+  vector<int>ans;
+    list<int>l;
+    int i=0,j=0;
+    int windowSize = j-i+1;
+    while(j<n){
+            while (!l.empty() && arr[j] > l.back()) {
+                l.pop_back();
+            }
+            l.push_back(arr[j]);
+            if (j - i + 1 < k)
+                j++;
+            else if (j - i + 1 == k) {
+                ans.push_back(l.front());
+                if (l.front() == arr[i])
+                    l.pop_front();
+                i++;
+                j++;
+            }
+    }
+    return ans;
+}
+};
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+        vector<int> res = ob.max_of_subarrays(arr, n, k);
+        for (int i = 0; i < res.size(); i++) cout << res[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+} 
